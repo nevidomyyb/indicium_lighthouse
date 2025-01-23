27f903b1420f57d8bb94d4913401b4be660faeb2
Pedro Cunha <idk.pedroc@gmail.com>
2025-01-22 22:27:13 -0300

    add: NaN fixed

--- indicium_lighthouse/EDA.py
+++ indicium_lighthouse/EDA.py
@@ -5,4 +5,10 @@ data = pd.read_csv('./indicium_lighthouse/teste_indicium_precificacao.csv')
 df_nan = data[data.isna().any(axis=1)]
 nan_counts = df_nan.isna().sum()
 df_nan_counts = pd.DataFrame({'coluna': nan_counts.index, "quantidade": nan_counts.values})
-print(df_nan_counts)
+
+data = data.fillna(value={
+    'nome': 'Não informado',
+    'host_name': 'Não informado',
+    'ultima_review': pd.NaT,
+    'reviews_por_mes': 0
+})
